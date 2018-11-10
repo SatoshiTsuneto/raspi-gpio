26b117ab760b8c61d5243b987f39a0437a893d2a
satotune <32587994+satotune@users.noreply.github.com>
2018-11-10 12:32:43 +0900

    冗長な部分の修正

--- mygpio.c
+++ mygpio.c
@@ -6,18 +6,19 @@
 #include <time.h>
 
 /* 定数宣言 */
-// 各状態のアドレス
+/* 各状態のアドレス */
 #define BLOCK_SIZE      4 * 1024
 #define GPIO_BASE       0x3F200000
 #define SET 7 // HIGH OUTPUT
 #define CLEAR 10 // LOW OUTPUT
 #define GET 13 // INPUT
 
-// あれば状態がわかりやすくなるので定義
+/* あれば状態がわかりやすくなるので定義 */
 enum {
     LOW, HIGH, INPUT, OUTPUT
 };
-// 構造体
+
+/* 構造体 */
 typedef struct {
     unsigned long gpio_base;
     int memory_fd;
@@ -25,8 +26,8 @@ typedef struct {
     volatile unsigned int *addr;
 } rpi_gpio;
 
-//-----// 以下　関数群 //-----//
-/*マップ設定 */
+/* 以下　関数群 */
+/* マップ設定 */
 int mapGPIO(rpi_gpio *gpio) {
     gpio->memory_fd = open("/dev/mem", O_RDWR | O_SYNC);
     if (gpio->memory_fd < 0) {
@@ -55,19 +56,7 @@ void unmapGPIO(rpi_gpio *gpio) {
     close(gpio->memory_fd);
 }
 
-/* 現在時刻の表示 */
-void nowTimeDisp(void) {
-    time_t t;
-    struct tm *t_st;
-    /* 現在時刻の取得 */
-    time(&t);
-    /* 現在時刻を構造体に変換 */
-    t_st = localtime(&t);
-    printf("%02d時", t_st->tm_hour);
-    printf("%02d分", t_st->tm_min);
-    printf("%02d秒\n", t_st->tm_sec);
-}
-
+/* GPIOピンの設定 */
 void pinMode(rpi_gpio *gpio, int pin, int staus) {
     int ten_value = pin / 10;
     int one_value = pin % 10;
@@ -83,7 +72,7 @@ int gpioRead(rpi_gpio *gpio, int pin) {
     return !((*(gpio->addr + GET) & (1 << pin)));
 }
 
-/*   出力処理 (HIGH & LOW) */
+/* 出力処理 (HIGH & LOW) */
 void gpioWrite(rpi_gpio *gpio, int pin, int status) {
     if (status == HIGH) {
         *(gpio->addr + SET) = (unsigned int) (1 << pin);
@@ -98,13 +87,12 @@ void cleanup(rpi_gpio *gpio, int pin) {
     *(gpio->addr + ten_value) = 0x00000000;
 }
 
-//-----// メイン処理 //-----//
+/* メイン処理 */
 int main(void) {
-    rpi_gpio gpio = {GPIO_BASE};
-    int map_status;
-    map_status = mapGPIO(&gpio);
+    rpi_gpio gpio = { GPIO_BASE };
+    int map_status = mapGPIO(&gpio);
     if (map_status) {
-        printf("Failed.\n");
+        printf("Failed to GPIO Setting.\n");
         return map_status;
     }
     /* ここにメイン処理を書く */
